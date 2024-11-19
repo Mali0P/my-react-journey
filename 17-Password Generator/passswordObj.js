b88197117bb69694dc929d83e06f1857eccd2d29
Pranesh Mali <pranesh.malakar.1@gmail.com>
2024-11-19 17:10:16 +0545

    Delete 17-Password Generator directory

--- 17-Password Generator/passswordObj.js
+++
@@ -1,4 +0,0 @@
-export let uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-export let lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-export let symbols = "!@#$%^&*()_+-=[]{}|;:',.<>?";
-export let numbersStore = "0123456789";
